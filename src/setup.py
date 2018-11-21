e872a0b4ca719e99c1ed8b2ba287b0c4bd00b38a
Richie Bendall <richiebendall@gmail.com>
2018-11-21 16:58:04 +1300

    Added files

--- src/setup.py
+++ src/setup.py
@@ -1,1 +1,28 @@
+# Import setuptools module
+import setuptools
 
+# Parse the readme markdown file
+with open("../README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="quilt-lang",
+    version="0.0.0",
+    author="Richie Bendall",
+    author_email="richiebendall@gmail.com",
+    description=
+    "A Python library that lets you write less code to do more things.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Richienb/quilt",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ),
+)
