93714914a91c46630480d666aafb902d475a75e4
Richie Bendall <richiebendall@gmail.com>
2018-11-22 20:47:39 +1300

    Bump minor version

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="quilt-lang",
-    version="0.1.14",
+    version="0.2.0",
     author="Richie Bendall",
     author_email="richiebendall@gmail.com",
     description=
