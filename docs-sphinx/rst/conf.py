60e86869e8ae452e4153ba1f256d2c8e47892181
Richie Bendall <richiebendall@gmail.com>
2018-12-15 15:13:29 +1300

    Fix inline code problem (Fixes: #3)

--- docs-sphinx/rst/conf.py
+++ docs-sphinx/rst/conf.py
@@ -36,9 +36,7 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.doctest'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -103,9 +101,9 @@ html_theme_options = {
     # https://getmdl.io/customize/index.html
     #
     # Values: amber, blue, brown, cyan deep_orange, deep_purple, green, grey, indigo, light_blue,
-    #         light_green, lime, orange, pink, purple, red, teal, yellow(Default: indigo)
+    # light_green, lime, orange, pink, purple, red, teal, yellow (Default: indigo)
     'primary_color': 'indigo',
-    # Values: Same as primary_color. (Default: pink)
+    # Values: Same as primary color. (Default: pink)
     'accent_color': 'red',
 
     # Customize layout.
@@ -149,7 +147,7 @@ html_static_path = ['_static']
 
 # -- Options for LaTeX output ------------------------------------------------
 
-#latex_elements = {
+# latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #
 # 'papersize': 'letterpaper',
@@ -165,35 +163,35 @@ html_static_path = ['_static']
 # Latex figure (float) alignment
 #
 # 'figure_align': 'htbp',
-#}
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-#latex_documents = [
+# latex_documents = [
 #    (master_doc, 'Quilt.tex', 'Quilt Documentation',
 #     'Richie Bendall', 'manual'),
-#]
+# ]
 
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-#man_pages = [
+# man_pages = [
 #    (master_doc, 'quilt', 'Quilt Documentation',
 #     [author], 1)
-#]
+# ]
 
 # -- Options for Texinfo output ----------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-#texinfo_documents = [
+# texinfo_documents = [
 #    (master_doc, 'Quilt', 'Quilt Documentation',
 #     author, 'Quilt', 'One line description of project.',
 #     'Miscellaneous'),
-#]
+# ]
 
 # -- Options for Epub output -------------------------------------------------
 
